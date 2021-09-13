78debdfd66844bfd3ac04b1cdc9e9ae32cf2685f
Andrey Okunev <endryondrop@gmail.com>
2021-09-13 09:08:31 +0300

    Added lab1

---
+++ lab1/task1/task1/task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "SimUDuck.cpp"
+
+using namespace std;
+
+void DrawDuck(Duck const& duck)
+{
+	duck.Display();
+}
+
+void PlayWithDuck(Duck& duck)
+{
+	DrawDuck(duck);
+	duck.Quack();
+	duck.Fly();
+	duck.Dance();
+	cout << endl;
+}
+
+int main()
+{
+	ModelDuck modelDuck;
+	PlayWithDuck(modelDuck);
+	modelDuck.SetFlyBehavior(make_unique<FlyWithWings>());
+	modelDuck.SetDanceBehavior(make_unique<MinuetBehavior>());
+	PlayWithDuck(modelDuck);
+
+	return 0;
+}
